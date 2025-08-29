d5784ca7e0de2dcb9bd929a1f39f543b222a0812
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-08-28 23:18:37 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     },
     conditions: ['import', 'module', 'browser', 'default']
   },
-          base: "/PrimalLifts/",
+  base: "/PrimalLifts/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
